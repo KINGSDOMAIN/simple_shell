4b40e6ae0faa70d83ac674367b222b8c93337999
kingsley <kingsdomain50@gmail.com>
2023-12-12 20:22:33 -0800

    simple betty checks

---
+++ betty_check.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * printmsg - a simple shell program
+ * Discription: this function displays a message to the standard output
+ */
+
+void printmsg(void)
+{
+	printf("This message to the output stream of the computer. \n");
+}
+
+/**
+ * main - entry point
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	printmsg();
+	return (0);
+}
+
