9bb4c490fa48782e7c27af7ef1d9c4167887fb3e
kingsley <kingsdomain50@gmail.com>
2023-12-13 17:20:24 -0800

    fixed

--- betty_check.c
+++ betty_check.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
 /**
-* printmsg - a simple shell program
-* Discription: this function displays a message to the standard output
-*/
-
+ * printmsg - a simple shell program
+ * Discription: this function displays a message to the standard output
+ */
 void printmsg(void)
 {
+	char *message = "This message to the output stream of the computer \n";
 
-char *message = "This message to the output stream of the computer \n";
-while (*message != '\0')
-{
-putchar(*message);
-message++;
-}
+	while (*message != '\0')
+	{
+		putchar(*message);
+		message++;
+	}
 }
 
 /**
-* main - entry point
-* Return: 0 on success Always
-*/
-
+ *  main - entry point
+ * Return: 0 on success Always
+ */
 int main(void)
 {
-printmsg();
-return (0);
+	printmsg();
+	return (0);
 }
